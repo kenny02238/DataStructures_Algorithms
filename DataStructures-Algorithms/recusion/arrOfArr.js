d027ffdb2d2164892fab3511a1ba74d1fb840e48
Kenny <asn>
2023-11-02 16:24:35 +0800

    test

--- DataStructures-Algorithms/recusion/arrOfArr.js
+++ DataStructures-Algorithms/recusion/arrOfArr.js
@@ -3,7 +3,6 @@ let arrs = [[[["a", [["b", ["c"]], ["d"]]], [["e"]], [[["f", "g", "h"]]]]]];
 function collector(arr1) {
   let result = [];
   helper(arr1);
-  console.log(result);
 
   return result;
 
