8ab971e14fe4e65849c4d705707013f0822f2948
Kenny <kenny02238@yahoo.com.tw>
2023-11-06 16:20:45 +0800

    fix: folder structrue

---
+++ DataStructures-Algorithms/Tree/MST.js
@@ -0,0 +1,6 @@
+// Minimal Spanning Tree: is a subset of the edges of a connected,
+// edge-weighted undirected graph that connects all the vertices together,
+// without any cycles and with the minimum possible total edge weight.
+// For a graph with V vertices, a spanning tree is a subset of V-1 edges
+// However, the minimal spanning tree is one whose edge have the smallest total weight among all the spanning trees.
+// (This assume a weighted graph; i.e., a graph in which each edge has a weight or cost associated with it.)
