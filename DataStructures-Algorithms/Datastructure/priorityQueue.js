edfd0b0c6e621f72c37741a93f29ec5ea3059f51
Kenny <kenny02238@yahoo.com.tw>
2023-11-06 11:24:46 +0800

    docs: add priority queue definition

---
+++ DataStructures-Algorithms/Datastructure/priorityQueue.js
@@ -0,0 +1,6 @@
+// In Computer Science, a priority queue is an abstract data type which is like a regular queue or stack data structure,
+// but where additionally each element has a "priority" associated with it.
+// Elements with higher priority are served before elements with lower priority.
+
+// We can implement it with a linked list, queue, array, or other data structure.
+// But... Max Heap is the most efficient
